6c2e23e77c7ad3196305239d84e1ed3d0c7ed100
yds05116 <yds05116@naver.com>
2021-05-08 15:15:54 +0900

    1541

---
+++ BOJ/그리디/1541.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c[51];
+    cin>>c;
+    for(int i=0; i<sizeof(c); i++)
+    {
+        if(c[i]=='-')
+        {
+            c[i]=
+        }
+    }
+    return 0;
+}
